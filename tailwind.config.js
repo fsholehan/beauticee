3b4d8f0d9e9eba06a45a29005ee3d9ed9936486b
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-01-10 08:03:40 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          10: "#414880",
+          15: "#091156",
+        },
+        gray: {
+          10: "#8B8B8B",
+        },
+        pink: {
+          10: "#FF64AE",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
